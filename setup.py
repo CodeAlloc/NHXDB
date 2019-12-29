9a28e06c73639a8afee1cc86d43d1ec3a00d9d03
Ch. Muhammad Sohaib <35073175+chmuhammadsohaib@users.noreply.github.com>
2019-12-30 02:40:09 +0500

    V1.0 Stable Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NHXDB",
-    version="1.0",
+    version="1.0.0",
     author="Ch. Muhammad Sohaib",
     author_email="chmuhammadsohaib@gmail.com",
     description="A lightweight Database Module with a blend of SQL like Language and ORM syntax",
