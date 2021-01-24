db3c1e7cf234afdaad7c6d885f172fd1b98e85f3
Muhammad Sohaib Shahzad <35073175+chmuhammadsohaib@users.noreply.github.com>
2021-01-24 17:29:04 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NHXDB",
-    version="v1.2.1",
+    version="v1.2.2",
     author="NHXTech",
     author_email="chmuhammadsohaib@gmail.com",
     description="A lightweight Database Module with a blend of SQL Language and ORM syntax",
